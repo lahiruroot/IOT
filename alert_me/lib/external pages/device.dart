2da0344dcb79dfdac481e382ebe4ccf96f74860f
Malaka98 <malaka.aruna14413@gmail.com>
2021-05-10 14:16:30 +0530

    fix

--- alert_me/lib/external pages/device.dart
+++ alert_me/lib/external pages/device.dart
@@ -7,22 +7,22 @@ import 'package:http/http.dart' as http;
 
 Future<List<Devide>> fetchPhotos(http.Client client) async {
   final response =
-      await client.get(Uri.parse('https://hopeful-wing-87181c.netlify.app'));
-
+      await client.get(Uri.parse('https://nadun.smartqrlite.shop/json.php'));
+  // print(response);
   return compute(parsePhotos, response.body);
 }
 
 // A function that converts a response body into a List<Photo>.
 List<Devide> parsePhotos(String responseBody) {
-  List fd = [];
-  Map<String, dynamic> x = jsonDecode(responseBody);
-  // print(x.keys);
-  x.forEach((key, value) {
-    // print(key);
-    // print(value);
-    fd.add(value);
-  });
-  final parsed = fd.cast<Map<String, dynamic>>();
+  // List fd = [];
+  // Map<String, dynamic> x = jsonDecode(responseBody);
+  // // print(x.keys);
+  // x.forEach((key, value) {
+  //   // print(key);
+  //   // print(value);
+  //   fd.add(value);
+  // });
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
   // print(parsed);
   return parsed.map<Devide>((json) => Devide.fromJson(json)).toList();
 }
@@ -31,14 +31,16 @@ class Devide {
   final String temp;
   final String h;
   final String m;
+  final String t;
 
-  Devide({this.temp, this.m, this.h});
+  Devide({this.temp, this.m, this.h, this.t});
 
   factory Devide.fromJson(Map<String, dynamic> json) {
     return Devide(
-      temp: json['temp'] as String,
-      h: json['h'] as String,
-      m: json['m'] as String,
+      temp: json['value1'] as String,
+      h: json['value2'] as String,
+      m: json['value3'] as String,
+      t: json['reading_time'] as String,
     );
   }
 }
@@ -70,21 +72,36 @@ class _MyDeviceState extends State<MyDevice> {
                   return Card(
                     child: Container(
                       padding: EdgeInsets.all(20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      child: Column(
+                        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            "Temp: ${fd[index].temp}",
-                            style: TextStyle(fontSize: 20.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Temp: ${fd[index].temp}",
+                                style: TextStyle(fontSize: 15.0),
+                              ),
+                              Text(
+                                "Humidity: ${fd[index].h}",
+                                style: TextStyle(fontSize: 15.0),
+                              ),
+                            ],
                           ),
-                          Text(
-                            "Humidity: ${fd[index].h}",
-                            style: TextStyle(fontSize: 20.0),
-                          ),
-                          Text(
-                            "Motion: ${fd[index].m}",
-                            style: TextStyle(fontSize: 20.0),
-                          ),
+                          Divider(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Motion: ${fd[index].m}",
+                                style: TextStyle(fontSize: 15.0),
+                              ),
+                              Text(
+                                "Time: ${fd[index].t}",
+                                style: TextStyle(fontSize: 15.0),
+                              ),
+                            ],
+                          )
                         ],
                       ),
                     ),
